e666bd79a5708d5045e6e154cef6ead3d235ac38
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-08-20 00:26:31 +0530

    one-to -one

---
+++ orm-mapping-demo/src/main/java/com/tech/entity/Address.java
@@ -0,0 +1,21 @@
+package com.tech.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "address_id")
+	private Long addressId;
+	
+	@Column(name = "country")
+	private String country;
+}
