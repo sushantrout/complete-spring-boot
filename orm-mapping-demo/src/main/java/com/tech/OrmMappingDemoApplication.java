049648d0590a3c59924954159973779ac52ac416
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-08-19 23:17:20 +0530

    initial commit
    initial commit

---
+++ orm-mapping-demo/src/main/java/com/tech/OrmMappingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmMappingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmMappingDemoApplication.class, args);
+	}
+
+}
