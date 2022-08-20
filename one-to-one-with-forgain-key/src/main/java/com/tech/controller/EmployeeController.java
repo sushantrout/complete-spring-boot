e2d56f7bf0e916de1d7c20f12f896e73ac91b71e
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2022-08-20 09:24:57 +0530

    one-to-one-model-with-forgain-key
    one-to-one-model-with-forgain-key

---
+++ one-to-one-with-forgain-key/src/main/java/com/tech/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.tech.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tech.entity.Employee;
+import com.tech.service.EmployeeService;
+
+@RestController
+@RequestMapping(value = "employee")
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeService employeeService;
+	
+	@PostMapping
+	public Employee create(Employee employee) {
+		return employeeService.create(employee);
+	}
+}
---
+++ one-to-one-with-forgain-key/src/main/java/com/tech/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.tech.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_employee")
+@Data
+public class Employee {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "employee_name")
+	private String name;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id", referencedColumnName = "id")
+	private Address address;
+
+}
